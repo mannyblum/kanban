66c553bbbaf4a2d3771da65029cb1de3f30c0641
Manny <manny.blum@gmail.com>
2025-09-08 12:52:41 -0400

    moving modal to its own component

---
+++ src/components/Modals/ColumnModal.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
+
+import classes from "../../components/projectboard.module.css";
+
+import { type Column } from "../../../lib/columns";
+
+interface ColumnModalProps {
+  onClose: () => void;
+  onAddColumn: (columnName: string) => void;
+  onEditColumn: (column: Column) => void;
+  column?: Column | null;
+}
+
+export default function ColumnModal({
+  onClose,
+  onAddColumn,
+  onEditColumn,
+  column,
+}: ColumnModalProps) {
+  const [columnName, setColumnName] = useState<string>("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setColumnName(event.target?.value);
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (column) {
+      const newColumn: Column = {
+        ...column,
+        title: columnName,
+      };
+
+      onEditColumn(newColumn);
+    } else {
+      onAddColumn(columnName);
+    }
+  };
+
+  useEffect(() => {
+    if (column) {
+      setColumnName(column.title);
+    }
+  }, [column]);
+
+  return (
+    <dialog className={classes.dialogWrapper}>
+      <div className={classes.backdrop} />
+      <div className={classes.dialog}>
+        <div className={classes.dialogPanel}>
+          <div className={classes.dialogPanelContent}>
+            <h3 className={classes.dialogTitle}>
+              {column ? "Edit" : "Add New"} Column
+            </h3>
+            <form onSubmit={onSubmit}>
+              <div className={classes.formControl}>
+                <label htmlFor="columnTitle" className={classes.label}>
+                  Column Title
+                </label>
+                <input
+                  type="text"
+                  id="columnTitle"
+                  name="columnTitle"
+                  onChange={handleChange}
+                  value={columnName}
+                  className={classes.input}
+                  placeholder="Enter column title..."
+                />
+              </div>
+              <div className={classes.dialogFooter}>
+                <button
+                  type="button"
+                  onClick={onClose}
+                  className={classes.cancel}
+                >
+                  Cancel
+                </button>
+                <button type="submit" className={classes.primary}>
+                  {column ? "Edit" : "Add"} Column
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </dialog>
+  );
+}
--- src/components/ProjectBoard.tsx
+++ src/components/ProjectBoard.tsx
@@ -1,10 +1,4 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  type ChangeEvent,
-  type FormEvent,
-} from "react";
+import { useEffect, useRef, useState } from "react";
 import classes from "./projectboard.module.css";
 import { createPortal } from "react-dom";
 import {
@@ -14,95 +8,14 @@ import {
   deleteColumn,
   type Column,
 } from "../../lib/columns";
-import { useNotifications } from "../hooks/useNotifications";
 
+import { useNotifications } from "../hooks/useNotifications";
 import type { Notification } from "../context/notifications";
-import { Column as ProjectColumn } from "./ProjectColumns";
+
 import useConfirm from "../hooks/useConfirm";
 
-interface ColumnModalProps {
-  onClose: () => void;
-  onAddColumn: (columnName: string) => void;
-  onEditColumn: (column: Column) => void;
-  column?: Column | null;
-}
-
-export function ColumnModal({
-  onClose,
-  onAddColumn,
-  onEditColumn,
-  column,
-}: ColumnModalProps) {
-  const [columnName, setColumnName] = useState<string>("");
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setColumnName(event.target?.value);
-  };
-
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    if (column) {
-      const newColumn: Column = {
-        ...column,
-        title: columnName,
-      };
-
-      onEditColumn(newColumn);
-    } else {
-      onAddColumn(columnName);
-    }
-  };
-
-  useEffect(() => {
-    if (column) {
-      setColumnName(column.title);
-    }
-  }, [column]);
-
-  return (
-    <dialog className={classes.dialogWrapper}>
-      <div className={classes.backdrop} />
-      <div className={classes.dialog}>
-        <div className={classes.dialogPanel}>
-          <div className={classes.dialogPanelContent}>
-            <h3 className={classes.dialogTitle}>
-              {column ? "Edit" : "Add New"} Column
-            </h3>
-            <form onSubmit={onSubmit}>
-              <div className={classes.formControl}>
-                <label htmlFor="columnTitle" className={classes.label}>
-                  Column Title
-                </label>
-                <input
-                  type="text"
-                  id="columnTitle"
-                  name="columnTitle"
-                  onChange={handleChange}
-                  value={columnName}
-                  className={classes.input}
-                  placeholder="Enter column title..."
-                />
-              </div>
-              <div className={classes.dialogFooter}>
-                <button
-                  type="button"
-                  onClick={onClose}
-                  className={classes.cancel}
-                >
-                  Cancel
-                </button>
-                <button type="submit" className={classes.primary}>
-                  {column ? "Edit" : "Add"} Column
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </dialog>
-  );
-}
+import ColumnModal from "./Modals/ColumnModal";
+import { Column as ProjectColumn } from "./ProjectColumns";
 
 export default function ProjectBoard() {
   const [showColumnModal, setShowColumnModal] = useState<boolean>(false);
